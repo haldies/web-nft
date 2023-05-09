7ff23a23c6000b605420f9e6b6fb063025d02fec
Haldies Gerhardien Pasya <81854080+haldies@users.noreply.github.com>
2023-05-09 15:45:17 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,202 @@
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixednav = header.offsetTop;
+
+    if(window.pageYOffset > fixednav){
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+};
+
+const hamberger = document.querySelector('#hamberger');
+const navMenu = document.querySelector('#nav-nemu');
+
+hamberger.addEventListener('click', function(){
+    hamberger.classList.toggle('hamberger-aktive');
+    navMenu.classList.toggle('hidden');
+});
+
+
+window.addEventListener('scroll', (e) => {
+  document.querySelector('#spin').style.transform = `rotate(${window.scrollY}deg)`;
+
+
+  let spin = document.querySelector('#spin');
+
+  function animateOnScroll() {
+    let scrollY = window.scrollY;
+    spin.style.transform = `rotate(${scrollY}deg)`;
+    requestAnimationFrame(animateOnScroll);
+  }
+  
+  animateOnScroll();
+  
+  const section5 = document.querySelector('#roadmap');
+  const mapItems = document.querySelectorAll('.map-item');
+  
+  function handleScroll() {
+    const mapItemsSatu = document.querySelectorAll('.map-item-satu');
+    const screenWidth = window.innerWidth;
+    let translateX = -0;
+  
+    if (screenWidth >= 1024) {
+      translateX = (window.scrollY - section5.getBoundingClientRect().top) / 25;
+      mapItems.forEach((mapItem) => {
+        mapItem.classList.remove('translate-left');
+        mapItem.classList.add('translate-right');
+      });
+      mapItemsSatu.forEach((mapItem) => {
+        mapItem.classList.remove('translate-right');
+        mapItem.classList.add('translate-left');
+      });
+    } else if (screenWidth >= 768) {
+      translateX = (window.scrollY - section5.getBoundingClientRect().top) / 30;
+      mapItems.forEach((mapItem) => {
+        mapItem.classList.remove('translate-right');
+        mapItem.classList.remove('translate-left');
+      });
+      mapItemsSatu.forEach((mapItem) => {
+        mapItem.classList.remove('translate-left');
+        mapItem.classList.remove('translate-right');
+      });
+    } else {
+      translateX = (window.scrollY - section5.getBoundingClientRect().top) / 35;
+      mapItems.forEach((mapItem) => {
+        mapItem.classList.remove('translate-right');
+        mapItem.classList.add('translate-left');
+      });
+      mapItemsSatu.forEach((mapItem) => {
+        mapItem.classList.remove('translate-left');
+        mapItem.classList.add('translate-right');
+      });
+    }
+  
+    if (translateX > 0) {
+      if (screenWidth < 768) {
+        mapItems.forEach((mapItem) => {
+          mapItem.style.transform = `translateX(${translateX}px)`;
+        });
+        mapItemsSatu.forEach((mapItem) => {
+          mapItem.style.transform = `translateX(-${translateX}px)`;
+        });
+      } else {
+        mapItems.forEach((mapItem) => {
+          mapItem.style.transform = `translateX(-${translateX}px)`;
+        });
+        mapItemsSatu.forEach((mapItem) => {
+          mapItem.style.transform = `translateX(${translateX}px)`;
+        });
+      }
+    }
+  }
+  
+  function animateMapItems() {
+    window.requestAnimationFrame(handleScroll);
+  }
+  
+  window.addEventListener('scroll', animateMapItems);
+
+});
+
+// Definisikan fungsi untuk menambahkan event listener pada elemen clipboard
+function addClipboardListener(clipboard) {
+  clipboard.addEventListener("click", function() {
+    // Buat elemen input yang tidak terlihat pada halaman web
+    var input = document.createElement("input");
+    input.style.position = "fixed";
+    input.style.left = "-9999px";
+    input.value = "0x00000000000000000000000Dead";
+    document.body.appendChild(input);
+
+    // Pilih dan salin teks pada elemen input
+    input.select();
+    document.execCommand("copy");
+
+    // Hapus elemen input dari halaman web
+    document.body.removeChild(input);
+
+    // Tampilkan pesan berhasil menyalin teks ke clipboard
+    var popup = document.createElement("h4");
+    popup.textContent = "Copied";
+    popup.classList.add("w-20", "h-10", "text-center", "flex", "items-center", "justify-center", "bg-green-500", "rounded-24","absolute","-top-10","text-xs");
+    clipboard.appendChild(popup);
+
+    // Hilangkan popup setelah 2 detik
+    setTimeout(function() {
+      clipboard.removeChild(popup);
+    }, 800);
+  });
+}
+
+// Cari elemen-elemen "p" dengan id "clipboard"
+var clipboard1 = document.getElementById("clipboard1");
+var clipboard2 = document.getElementById("clipboard2");
+
+// Tambahkan event listener "click" pada masing-masing elemen clipboard
+addClipboardListener(clipboard1);
+addClipboardListener(clipboard2);
+
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(n) {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].classList.add("active");
+}
+
+document.querySelector("#prev").addEventListener("click", function() {
+  showSlide(currentSlide - 1);
+});
+
+document.querySelector("#next").addEventListener("click", function() {
+  showSlide(currentSlide + 1);
+});
+
+showSlide(currentSlide);
+
+
+const buttons = document.querySelectorAll('.btn');
+const images = document.querySelectorAll('.image');
+
+for(let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('mouseover', () => {
+    images[i].classList.add('translate-x-8');
+    buttons[i].style.cursor = 'pointer';
+  });
+
+  buttons[i].addEventListener('mouseout', () => {
+    images[i].classList.remove('translate-x-8');
+    buttons[i].style.cursor = 'default';
+  });
+
+  buttons[i].addEventListener('touchstart', () => {
+    images[i].classList.add('translate-x-8');
+    buttons[i].style.cursor = 'pointer';
+  });
+
+  buttons[i].addEventListener('touchend', () => {
+    images[i].classList.remove('translate-x-8');
+    buttons[i].style.cursor = 'default';
+  });
+}
+
+const scrollTopBtn = document.querySelector('#scroll-top-btn');
+
+// Menampilkan tombol scroll to top ketika user telah scroll sejauh 20% dari halaman
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > (window.innerHeight * 0.2)) {
+    scrollTopBtn.classList.remove('hidden');
+  } else {
+    scrollTopBtn.classList.add('hidden');
+  }
+});
+
+// Mengatur posisi scroll ke paling atas ketika tombol scroll to top diklik
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
